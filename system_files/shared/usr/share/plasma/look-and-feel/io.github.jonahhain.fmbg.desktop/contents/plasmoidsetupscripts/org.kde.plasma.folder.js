ae76abbcf4b257bfbd7a265d06c0bda64fbdff79
Jonah Hain <nope@nah.com>
2026-01-19 18:13:52 +0100

    Update wallpapers and accent color

--- system_files/shared/usr/share/plasma/look-and-feel/io.github.jonahhain.fmbg.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
+++ system_files/shared/usr/share/plasma/look-and-feel/io.github.jonahhain.fmbg.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
@@ -1,4 +1,4 @@
 applet.wallpaperPlugin = 'org.kde.image'
 applet.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"]
-applet.writeConfig("Image", "/usr/share/wallpapers/fmbg-ascii/contents/images/1920x1200.png")
+applet.writeConfig("Image", "/usr/share/wallpapers/fmbg/contents/images/1920x1080.png")
 applet.reloadConfig()
